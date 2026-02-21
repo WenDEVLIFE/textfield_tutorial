b51c034cb91755b1edaeed9fe5b274b35a314566
WenDEVLIFE <frouenmedinajr@gmail.com>
2026-02-21 13:43:05 +0800

    added backe nd functions

--- app/src/main/java/com/example/textfield_tutorial/MainActivity.java
+++ app/src/main/java/com/example/textfield_tutorial/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.textfield_tutorial;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,11 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+    private EditText usernameField, passwordField;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +27,23 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        usernameField = findViewById(R.id.editTextText2);
+        passwordField = findViewById(R.id.editTextTextPassword2);
+
+        button  = findViewById(R.id.button);
+        button.setOnClickListener(v -> {
+            String username = usernameField.getText().toString();
+            String password = passwordField.getText().toString();
+
+            if (username.isEmpty() || password.isEmpty()){
+                Toast.makeText(MainActivity.this, "Please fill in all the blanks", Toast.LENGTH_SHORT).show();
+            }
+            else{
+                Toast.makeText(MainActivity.this, "Login successful, Welcome User " + username + "Your Password is " + password , Toast.LENGTH_SHORT).show();
+            }
+
         });
     }
 }
